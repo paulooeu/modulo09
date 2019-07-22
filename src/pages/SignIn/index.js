6aca85b21ee0ebc9b1f57f34b7ffbab7e083bffd
Paulo <pro-solucoes@outlook.com>
2019-07-22 16:57:57 -0300

    Validação

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import logo from '~/assets/image/logo.svg';
+
+export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+  return (
+    <>
+      <img src={logo} alt="Logo" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta</Link>
+      </Form>
+    </>
+  );
+}
---
+++ src/routes/Route.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropsTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+import AuthLayout from '~/pages/_layout/auth';
+import DefaultLayout from '~/pages/_layout/default';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const signed = false;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
+}
+RouteWrapper.prototype = {
+  isPrivate: PropsTypes.bool,
+  component: PropsTypes.oneOfType([PropsTypes.element, PropsTypes.func])
+    .isRequired,
+};
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
