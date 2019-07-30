8984e6681507d51f922bfc9b3e99dceb9040105a
Paulo <pro-solucoes@outlook.com>
2019-07-30 12:53:20 -0300

    finalmente

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
-import Routes from './routes';
+
 import history from './services/history';
 import GlobalStyled from './styles/global';
 import './config/ReactrotronConfig';
 import store from './store';
+import { Router } from 'react-router-dom';
+import Routes from './routes';
 
 function App() {
   return (
